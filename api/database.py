0fd419fead2e81f09ae3bdf4da9d6be868804034
lj123123p <3904244120@qq.com>
2026-09-03 00:18:56 +0800

    Fix SQLite database path for Vercel serverless environment

--- api/database.py
+++ api/database.py
@@ -1,7 +1,7 @@
 ﻿from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///tmp/crypto_market.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:////tmp/crypto_market.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
